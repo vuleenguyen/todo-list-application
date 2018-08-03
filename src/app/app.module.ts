d90fbd7dcaadfa2d44dfc6413401da5859ff5f9a
Le, Nguyen <Nguyen.Le38@T-Mobile.com>
2018-08-02 21:01:20 -0500

    Set up routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
-
+import {Routes} from '@angular/router';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { SidebarComponent } from './core/sidebar/sidebar.component';
+import { UsersComponent } from './users/users.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TodosComponent } from './todos/todos.component';
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './core/about/about.component';
+import { ContactComponent } from './core/contact/contact.component';
+import { HomeComponent } from './core/home/home.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'users', component: UsersComponent
+  },
+  {
+    path: 'tasks', component: TasksComponent
+  },
+  {
+    path: 'todos', component: TodosComponent
+  }, 
+  {
+    path: 'about', component: AboutComponent  
+  }, 
+  {
+    path: 'contact', component: ContactComponent
+  },
+  {
+    path: '', component: HomeComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    UsersComponent,
+    TasksComponent,
+    TodosComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule, 
-    FormsModule 
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
